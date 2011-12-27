dc75f56bb794ccad3fe92e7add01641ed7c2d290
maemo <maemo@maemo-TomXue>
2011-12-27 14:28:31 +0000

    tidy design

--- trial_perror.c
+++ trial_perror.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+
 int main(void)
 {
- FILE *fp ;
- fp = fopen( "/root/noexitfile", "r+" );
- if ( NULL == fp )
- {
-  perror("/root/noexitfile wrong...");
- }
+	FILE *fp ;
+	fp = fopen( "/root/noexitfile", "r+" );
+	if ( NULL == fp )
+	{
+		perror("/root/noexitfile wrong...");
+	}
  
- return 0;
+	return 0;
 }
